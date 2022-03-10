d87ff45086db7ba7b022650b15e75cdb8c76dab8
Jon Schlipf <jon.schlipf@b-tu.de>
2022-03-10 18:14:32 +0100

    single particle fix

--- src/singleparticle.jl
+++ src/singleparticle.jl
@@ -1,10 +1,10 @@
-#MIE theory
+#Bohren-Huffman
 function α0(a,b,ε,εout=1,ε0=ε0)
 	if a==b #spherical case
 		L=1/3
 	else #L is depolarization factor
-		e=1-b^2*a^-2
-		g=√((1-e^2)*e^-2)
+		esquared=1-b^2*a^-2
+		g=√((1-esquared)/esquared)
 		L=g/(2*e^2)*(π/2-atan(g))-.5*g^2
 	end
 	return ε0*4π*a^2*b/3*(ε .-εout)./(εout .+L*(ε.-εout))
