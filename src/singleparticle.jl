dc44281d06c90a43469afe7e3e735be80aef5e61
Jon Schlipf <jon.schlipf@b-tu.de>
2022-03-14 10:30:15 +0100

    esquare

--- src/singleparticle.jl
+++ src/singleparticle.jl
@@ -5,7 +5,7 @@ function α0(a,b,ε,εout=1,ε0=ε0)
 	else #L is depolarization factor
 		esquared=1-b^2*a^-2
 		g=√((1-esquared)/esquared)
-		L=g/(2*e^2)*(π/2-atan(g))-.5*g^2
+		L=g/(2*esquared)*(π/2-atan(g))-.5*g^2
 	end
 	return ε0*4π*a^2*b/3*(ε .-εout)./(εout .+L*(ε.-εout))
 end
